ac029951d1a2d8a25ec31d3e71f467a8fe8969f8
Jeff Sheremata <jsheremat@users.noreply.github.com>
2016-07-14 22:28:59 -0500

    Update Clean_Text2.R

--- ngram_processing/Clean_Text2.R
+++ ngram_processing/Clean_Text2.R
@@ -5,6 +5,9 @@ Clean_Text2 <- function(textinput) {
   #text_sample1<--str_replace_all(text_sample1, pattern="\\s+", " ") 
   text_sample1 <-removeNumbers(text_sample1) 
   text_sample1 <-rm_stopwords(CT, stopwords =stopwords("english")) 
-  #text_sample1 <- 
+  text_sample1  
+  if (is.null(character_count(text_sample1))){
+    text_sample2<-"the"
+  }
   return (text_sample1)
 }
